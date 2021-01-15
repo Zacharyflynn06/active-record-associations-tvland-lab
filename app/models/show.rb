1f997b3da58dedeef3d27ec2c2c14544e393e5ba
Zac <zacharyflynn06@gmail.com>
2021-01-14 22:45:53 -0700

    commit tree cecc7e68fbf1bf23d840947037a0fcc503b67182

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
+
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+   
+
+        self.actors.collect do |i|
+            i.first_name + " " + i.last_name
+        end
+
+    end
   
 end
